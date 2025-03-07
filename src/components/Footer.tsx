328dff942908cfb7165aa7376ec4d440585b14a7
Adzlans <adzlansuharto94@gmail.com>
2025-03-07 12:46:21 +0800

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-dark-surface border-t border-dark-border py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-sm text-text-primary text-center font-light tracking-wide">
-          2025 @ sleepyadzlan - All Rights Reserved - Created by Adzlan
+          2025 @ SarangResepi - All Rights Reserved - Created by Adzlan
         </p>
       </div>
     </footer>
